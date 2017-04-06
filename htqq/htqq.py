3b4920d8f101e63bc646f7535620fb4df8b0a023
Io <me@me.me>
2017-04-06 20:23:54 +0200

    Redefine api

--- htqq/htqq.py
+++ htqq/htqq.py
@@ -37,7 +37,12 @@ def process(query, text):
         print(f"Err: {err}", file=sys.stderr)
         return
 
-    for x in tree.xpath(query):
+    y = tree.xpath(query)
+    if isinstance(y, str):
+        yield y
+        return
+
+    for x in y:
         if isinstance(x, str):
             x = x.strip()
         else:
@@ -72,6 +77,18 @@ def extract(query, xs):
             print(f"xpath '{query}': {err}", file=sys.stderr)
             exit(4)
 
+def split_pipeline(ql):
+    name, pipeline = None, []
+
+    for q in ql:
+        if q.endswith(":"):
+            yield name, pipeline
+            name, pipeline = q[:-1], []
+        else:
+            pipeline.append(q)
+
+    yield name, pipeline
+
 
 def do():
     args = docopt.docopt(__doc__, version=__version__)
@@ -84,15 +101,30 @@ def do():
     else:
         gen = [sys.stdin.read()]
 
-    for query in preprocess(query):
+    ps = split_pipeline(query)
+    ps = [(name, list(preprocess(p))) for name, p in ps]
+
+    initial_pipeline, ps = ps[0][1], ps[1:]
+    for query in initial_pipeline:
         gen = extract(query, gen)
 
     for x in gen:
         try:
-            if as_json:
-                json.dump(x, sys.stdout)
-            else:
-                sys.stdout.write(x)
+            d = x if not ps else {}
+            for field, pipeline in ps:
+                subgen = [x]
+                for subquery in pipeline:
+                    subgen = extract(subquery, subgen)
+
+                y = list(subgen)
+                if len(y) == 0:
+                    y = None
+                elif len(y) == 1:
+                    y = y[0]
+
+                d[field] = y
+
+            json.dump(d, sys.stdout)
             sys.stdout.write("\n")
             sys.stdout.flush()
         except:
